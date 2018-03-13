5921d6cfcd6bed84353b54e5622fc07b8927013f
Austin Nhung <austinnhung@yahoo.ca>
2018-03-13 01:09:56 -0600

    More work on plots and financial stuff

--- Assn2/apps.py
+++ Assn2/apps.py
@@ -25,6 +25,9 @@ plt.clf()
 
 # 2.4 Application to Financial Series
 # -------------------------------------
+
+# Part a: time series
+# - - - - - - - - - - -
 with open('phys581-stocks.txt', 'r') as file:
     stocks = [line.split() for line in file.readlines()[1:]]
 
@@ -32,47 +35,124 @@ stocks = np.array(list(map(list, zip(*stocks)))[1:], dtype=float)
 months = np.arange(0, len(stocks[0]))
 sandp, ford, gm, microsoft, sun, ustb3m = stocks
 
-plt.title('Company Stocks from 2002-2007')
-plt.xlabel('Months')
-plt.ylabel('Stock Price')
-plt.plot(months, sandp, label='SandP')
-plt.plot(months, ford, label='Ford')
-plt.plot(months, gm, label='GM')
-plt.plot(months, microsoft, label='Microsoft')
-plt.plot(months, sun, label='Sun')
-plt.plot(months, ustb3m, label='USTB3M')
-plt.legend(loc='best')
+fig, subs = plt.subplots(nrows=2, figsize=(8, 14), dpi=300)
+subs[0].set_title('SandP Stock Prices from 2002-2007')
+subs[0].set_xlabel('Time')
+subs[0].set_ylabel('Stock Price')
+subs[0].set_xlim(0, 65)
+subs[0].set_xticks(range(0, months.size, 12))
+subs[0].set_xticklabels([2002, 2003, 2004, 2005, 2006, 2007])
+subs[0].tick_params(axis='x', rotation=45)
+subs[0].plot(months, sandp)
+subs[0].grid()
+subs[1].set_title('Company Stocks from 2002-2007')
+subs[1].set_xlabel('Months')
+subs[1].set_ylabel('Stock Price')
+subs[1].set_xlim(0, 65)
+subs[1].set_ylim(0, 100)
+subs[1].set_xticks(range(0, months.size, 12))
+subs[1].set_xticklabels([2002, 2003, 2004, 2005, 2006, 2007])
+subs[1].tick_params(axis='x', rotation=45)
+subs[1].plot(months, ford, label='Ford')
+subs[1].plot(months, gm, label='GM')
+subs[1].plot(months, microsoft, label='Microsoft')
+subs[1].plot(months, sun, label='Sun')
+subs[1].plot(months, ustb3m, label='USTB3M')
+subs[1].grid()
+subs[1].legend(loc='best')
 plt.savefig('stocks_time')
 
+# Part b: compounded returns
+# - - - - - - - - - - - - - -
 returns = np.array(
-    [np.log(stocks[:,i] - stocks[:,i-1]) for i in range(1, months.size)]
+    [np.log(stocks[:,i] / stocks[:,i-1]) for i in range(1, months.size)]
 ).T
 months = np.arange(0, len(returns[0]))
 sandp, ford, gm, microsoft, sun, ustb3m = returns
 
-fig, subs = plt.subplots(nrows=2, ncols=3, figsize=(12, 8))
+fig, subs = plt.subplots(nrows=2, ncols=3, figsize=(14, 14), dpi=300)
+
 subs[0][0].set_title('SandP')
 subs[0][0].set_xlabel('Months')
 subs[0][0].set_ylabel('Continuously Compounded Returns')
+subs[0][0].set_xlim(0, 65)
+subs[0][0].set_xticks(range(0, months.size, 12))
+subs[0][0].set_xticklabels([2002, 2003, 2004, 2005, 2006, 2007])
+subs[0][0].tick_params(axis='x', rotation=45)
 subs[0][0].plot(months, sandp)
+subs[0][0].grid()
+
 subs[0][1].set_title('Ford')
 subs[0][1].set_xlabel('Months')
-subs[0][1].set_ylabel('Continuously Compounded Returns')
+subs[0][1].set_xlim(0, 65)
+subs[0][1].set_xticks(range(0, months.size, 12))
+subs[0][1].set_xticklabels([2002, 2003, 2004, 2005, 2006, 2007])
+subs[0][1].tick_params(axis='x', rotation=45)
 subs[0][1].plot(months, ford)
+subs[0][1].grid()
+
 subs[0][2].set_title('GM')
 subs[0][2].set_xlabel('Months')
-subs[0][2].set_ylabel('Continuously Compounded Returns')
+subs[0][2].set_xlim(0, 65)
+subs[0][2].set_xticks(range(0, months.size, 12))
+subs[0][2].set_xticklabels([2002, 2003, 2004, 2005, 2006, 2007])
+subs[0][2].tick_params(axis='x', rotation=45)
 subs[0][2].plot(months, gm)
+subs[0][2].grid()
+
 subs[1][0].set_title('Mircosoft')
 subs[1][0].set_xlabel('Months')
 subs[1][0].set_ylabel('Continuously Compounded Returns')
+subs[1][0].set_xlim(0, 65)
+subs[1][0].set_xticks(range(0, months.size, 12))
+subs[1][0].set_xticklabels([2002, 2003, 2004, 2005, 2006, 2007])
+subs[1][0].tick_params(axis='x', rotation=45)
 subs[1][0].plot(months, microsoft)
+subs[1][0].grid()
+
 subs[1][1].set_title('Sun')
 subs[1][1].set_xlabel('Months')
-subs[1][1].set_ylabel('Continuously Compounded Returns')
+subs[1][1].set_xlim(0, 65)
+subs[1][1].set_xticks(range(0, months.size, 12))
+subs[1][1].set_xticklabels([2002, 2003, 2004, 2005, 2006, 2007])
+subs[1][1].tick_params(axis='x', rotation=45)
 subs[1][1].plot(months, sun)
+subs[1][1].grid()
+
 subs[1][2].set_title('USTB3M')
 subs[1][2].set_xlabel('Months')
-subs[1][2].set_ylabel('Continuously Compounded Returns')
+subs[1][2].set_xlim(0, 65)
+subs[1][2].set_xticks(range(0, months.size, 12))
+subs[1][2].set_xticklabels([2002, 2003, 2004, 2005, 2006, 2007])
+subs[1][2].tick_params(axis='x', rotation=45)
 subs[1][2].plot(months, ustb3m)
+subs[1][2].grid()
+
 plt.savefig('stocks_returns')
+
+# Part c: autocorrelation
+# - - - - - - - - - - - - -
+
+# Part d: power spectrum
+# - - - - - - - - - - - -
+
+# Part e: dow jones
+# - - - - - - - - - -
+with open('phys581-dow.txt', 'r') as file:
+    dow = np.array(list(map(float, file.readlines())))
+
+t = np.arange(0, dow.size)
+freq = np.fft.fftfreq(t.size)
+dow_fft = np.abs(np.fft.fft(dow))**2
+
+fig, subs = plt.subplots(ncols=2)
+
+subs[0].plot(t, dow)
+
+subs[1].plot(freq, dow_fft)
+subs[1].set_yscale('log')
+
+plt.savefig('dow')
+
+# Part f: autocorrelation
+# - - - - - - - - - - - - -
